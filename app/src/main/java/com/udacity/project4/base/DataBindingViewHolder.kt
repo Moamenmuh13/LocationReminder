c6de761e741555cabbfd8ff7af0616c43a101da1
Mu <mumenmuhammad13@gmail.com>
2022-09-16 20:28:32 +0200

    Changes

--- app/src/main/java/com/udacity/project4/base/DataBindingViewHolder.kt
+++ app/src/main/java/com/udacity/project4/base/DataBindingViewHolder.kt
@@ -1,8 +1,8 @@
 package com.udacity.project4.base
 
 import androidx.databinding.ViewDataBinding
-import androidx.databinding.library.baseAdapters.BR
 import androidx.recyclerview.widget.RecyclerView
+import com.udacity.project4.BR
 
 /**
  * View Holder for the Recycler View to bind the data item to the UI
@@ -11,7 +11,7 @@ class DataBindingViewHolder<T>(private val binding: ViewDataBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: T) {
-        binding.setVariable(BR.item , item)
+        binding.setVariable(BR.item, item)
         binding.executePendingBindings()
     }
 }
